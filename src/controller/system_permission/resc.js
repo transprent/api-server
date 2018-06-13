ac32304ef0685164246fb2755ae5116c0317a6e8
kun <kun@example.com>
2018-06-13 18:03:22 +0800

    add

--- src/controller/system_permission/resc.js
+++ src/controller/system_permission/resc.js
@@ -8,7 +8,11 @@ exports.g_page = async (ctx) => {
   ctx.ok(d);
 };
 
-exports.p_page = async (ctx) => {
-  const d = await Model.SysResc.findAll();
-  ctx.ok(d);
-};
+exports = [
+  {
+    type: 'get',
+    path: 'add_add',
+    schema: {},
+    action() {},
+  },
+];
