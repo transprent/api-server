c4715011624fc06a94a237c7696874f056f9890c
仁焜 <renkun@appledeMacBook-Pro.local>
2018-08-07 19:57:22 +0800

    add

--- apps/admin/src/utils/log4js.js
+++ apps/admin/src/utils/log4js.js
@@ -1,43 +1,4 @@
-const log4js = require('log4js');
-
-log4js.configure({
-  replaceConsole: true,
-  appenders: {
-    console: { type: 'console' },
-    stdout: { type: 'stdout' },
-    error: {
-      type: 'dateFile',
-      filename: 'logs/error/',
-      pattern: 'yyyy-MM-dd.log',
-      maxLogSize: 10458760,
-      backups: 30,
-      alwaysIncludePattern: true,
-    },
-    http: {
-      type: 'dateFile',
-      filename: 'logs/http/',
-      pattern: 'yyyy-MM-dd.log',
-      maxLogSize: 10458760,
-      backups: 30,
-      alwaysIncludePattern: true,
-    },
-    default: {
-      type: 'dateFile',
-      filename: 'logs/default/',
-      pattern: 'yyyy-MM-dd.log',
-      maxLogSize: 10458760,
-      backups: 30,
-      alwaysIncludePattern: true,
-    },
-  },
-  categories: {
-    http: { appenders: ['stdout', 'console', 'http'], level: 'debug' },
-    error: { appenders: ['stdout', 'console', 'error'], level: 'error' },
-    default: { appenders: ['stdout', 'console', 'default'], level: 'info' },
-  },
-});
-
-// trace debug info warn error fatal
+const logger = require('../../../../common/lib/log4js');
 
 exports.getLogger = (category) => {
   return log4js.getLogger(category || 'default');
--- common/index.js
+++ common/index.js
@@ -5,4 +5,6 @@
 
 exports.Application = require('./lib/application');
 
+exports.log4js = require('./lib/utils/log4js');
+
 exports.utils = require('./lib/utils');
